cff669b54cbf07549071eff1d14888f35419fe68
triggs <thomas@itabs.nl>
2015-01-09 21:14:05 +0100

    Added GPLv3 license

--- SCTweak_cpp/AssemblyInfo.cpp
+++ SCTweak_cpp/AssemblyInfo.cpp
@@ -1,3 +1,20 @@
+/*************************************************************************
+    Copyright (C) 2015 trgs (https://github.com/trgs/)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*************************************************************************/
+
 #include "stdafx.h"
 
 using namespace System;
@@ -14,9 +31,9 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTitleAttribute("SCTweak")];
 [assembly:AssemblyDescriptionAttribute("")];
 [assembly:AssemblyConfigurationAttribute("")];
-[assembly:AssemblyCompanyAttribute("Microsoft")];
+[assembly:AssemblyCompanyAttribute("")];
 [assembly:AssemblyProductAttribute("SCTweak")];
-[assembly:AssemblyCopyrightAttribute("Copyright (c) Microsoft 2013")];
+[assembly:AssemblyCopyrightAttribute("GPLv3")];
 [assembly:AssemblyTrademarkAttribute("")];
 [assembly:AssemblyCultureAttribute("")];
 
